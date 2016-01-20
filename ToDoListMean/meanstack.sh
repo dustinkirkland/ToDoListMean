c50996b18fe3439a79d23de8e98df6a5b6cb3356
Anjay Ajodha <anajod@microsoft.com>
2016-01-19 23:28:48 -0800

    Quieted down apt

--- ToDoListMean/meanstack.sh
+++ ToDoListMean/meanstack.sh
@@ -3,18 +3,18 @@
 #Install node.js
 
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
-apt-get install -y nodejs
+apt-get -qq install -y nodejs
 
 #Install Git
-apt-get install -y git
+apt-get -qq install -y git
 
 #Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-apt-get update
+apt-get -qq update
 
-apt-get install -y mongodb-org
+apt-get -qq install -y mongodb-org
 
 service mongod start
 
